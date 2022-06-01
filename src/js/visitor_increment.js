67b9b158994b0faf5899bf4f02aa316c89a9a53b
Adrian <adriandefazio@outlook.com>
2022-06-01 13:43:20 +1000

    upd - fix same variable decl

--- src/js/visitor_increment.js
+++ src/js/visitor_increment.js
@@ -2,18 +2,17 @@ const address =
   "https://go42ln7xp0.execute-api.ap-southeast-2.amazonaws.com/serverless_lambda_prod/api/visitors";
 
 window.addEventListener("load", function () {
-  //everything is fully loaded, don't use me if you can use DOMContentLoaded
-  var xhr = new XMLHttpRequest();
-  xhr.open("PUT", address);
+  let xhr2 = new XMLHttpRequest();
+  xhr2.open("PUT", address);
 
-  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr2.setRequestHeader("Content-Type", "application/json");
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
+  xhr2.onreadystatechange = function () {
+    if (xhr2.readyState === 4) {
       console.log(xhr.status);
       console.log(xhr.responseText);
     }
   };
 
-  xhr.send();
+  xhr2.send();
 });
