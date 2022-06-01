298c996e5d4ccb07c8ff34522c401f06f8f85d38
Adrian <adriandefazio@outlook.com>
2022-06-01 13:49:47 +1000

    upd - adj PUT function

--- src/js/visitor_increment.js
+++ src/js/visitor_increment.js
@@ -1,16 +1,16 @@
-const address =
+const url_add =
   "https://go42ln7xp0.execute-api.ap-southeast-2.amazonaws.com/serverless_lambda_prod/api/visitors";
 
 window.addEventListener("load", function () {
   let xhr2 = new XMLHttpRequest();
-  xhr2.open("PUT", address);
+  xhr2.open("PUT", url_add);
 
   xhr2.setRequestHeader("Content-Type", "application/json");
 
   xhr2.onreadystatechange = function () {
     if (xhr2.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
+      console.log(xhr2.status);
+      console.log(xhr2.responseText);
     }
   };
 
