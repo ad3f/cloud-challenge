35950d2977ee7ff8a493b78da32452d33bf16572
Adrian <adriandefazio@outlook.com>
2022-06-01 13:23:41 +1000

    upd -increment script and gateway

--- src/js/visitor_increment.js
+++ src/js/visitor_increment.js
@@ -1,12 +1,16 @@
-const uri =
+const address =
   "https://go42ln7xp0.execute-api.ap-southeast-2.amazonaws.com/serverless_lambda_prod/api/visitors";
-let xhr = new XMLHttpRequest();
 
-//begin fetching our visitor count using our API
+var xhr = new XMLHttpRequest();
+xhr.open("PUT", address);
 
-$(document).ready(function () {
-  $.ajax({
-    type: "POST",
-    url: uri,
-  });
-});
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    console.log(xhr.status);
+    console.log(xhr.responseText);
+  }
+};
+
+xhr.send();
--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -3,7 +3,7 @@ resource "aws_apigatewayv2_api" "lambda" {
   protocol_type = "HTTP"
   cors_configuration {
     allow_origins = ["*"]
-    allow_methods = ["POST", "GET", "OPTIONS"]
+    allow_methods = ["POST", "GET", "OPTIONS", "PUT"]
     allow_headers = ["content-type"]
     max_age       = 300
   }
@@ -70,7 +70,7 @@ resource "aws_apigatewayv2_integration" "visitor_increment" {
 
 resource "aws_apigatewayv2_route" "visitor_increment" {
   api_id    = aws_apigatewayv2_api.lambda.id
-  route_key = "POST /api/visitors"
+  route_key = "PUT /api/visitors"
   target    = "integrations/${aws_apigatewayv2_integration.visitor_increment.id}"
 }
 
