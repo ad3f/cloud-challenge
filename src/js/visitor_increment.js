a4835893b39b69a69fbc8546fcc1be3b63219d56
Adrian <adriandefazio@outlook.com>
2022-06-01 13:30:15 +1000

    upd -surround func in window event listener

--- src/js/visitor_increment.js
+++ src/js/visitor_increment.js
@@ -1,16 +1,19 @@
 const address =
   "https://go42ln7xp0.execute-api.ap-southeast-2.amazonaws.com/serverless_lambda_prod/api/visitors";
 
-var xhr = new XMLHttpRequest();
-xhr.open("PUT", address);
+window.addEventListener("load", function () {
+  //everything is fully loaded, don't use me if you can use DOMContentLoaded
+  var xhr = new XMLHttpRequest();
+  xhr.open("PUT", address);
 
-xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Content-Type", "application/json");
 
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    console.log(xhr.status);
-    console.log(xhr.responseText);
-  }
-};
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+    }
+  };
 
-xhr.send();
+  xhr.send();
+});
